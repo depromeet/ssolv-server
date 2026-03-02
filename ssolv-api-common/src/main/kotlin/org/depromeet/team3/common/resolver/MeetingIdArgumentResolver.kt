1b9bcac249cbfb57e5afedf18af8bc3c87643028
parkmineum <a01087164428@gmail.com>
2026-03-02 18:48:54 +0900

    [FIX] : @MeetingId 오류 해결

--- ssolv-api-common/src/main/kotlin/org/depromeet/team3/common/resolver/MeetingIdArgumentResolver.kt
+++ ssolv-api-common/src/main/kotlin/org/depromeet/team3/common/resolver/MeetingIdArgumentResolver.kt
@@ -1,6 +1,8 @@
 package org.depromeet.team3.common.resolver
 
 import org.depromeet.team3.common.annotation.MeetingId
+import org.depromeet.team3.meeting.exception.MeetingException
+import org.depromeet.team3.common.exception.ErrorCode
 import org.depromeet.team3.util.MeetingIdParser
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -37,7 +39,15 @@ class MeetingIdArgumentResolver : HandlerMethodArgumentResolver {
         val value = pathVariables?.get(name)?.toString() 
             // 2. RequestParams에서 찾기 (Path에 없으면)
             ?: webRequest.getParameter(name)
-            ?: return null
+
+        if (value == null) {
+            val isNullable = parameter.parameterType == Long::class.javaObjectType ||
+                            parameter.parameterAnnotations.any { it.annotationClass.simpleName == "Nullable" }
+            if (isNullable) return null
+            
+            // 필수 파라미터인데 누락된 경우
+            throw MeetingException(ErrorCode.MISSING_PARAMETER)
+        }
 
         return MeetingIdParser.parse(value)
     }
