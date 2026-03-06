6acacde7433729157c46f85e9089947a7fbdbb1b
parkmineum <a01087164428@gmail.com>
2026-03-06 21:45:19 +0900

    [FEATURE] #156 : 알림 설정 기능 추가

---
+++ ssolv-api-core/src/main/kotlin/org/depromeet/team3/notification/dto/NotificationSettingResponse.kt
@@ -0,0 +1,5 @@
+package org.depromeet.team3.notification.dto
+
+data class NotificationSettingResponse(
+    val notificationEnabled: Boolean
+)
---
+++ ssolv-api-core/src/main/kotlin/org/depromeet/team3/notification/dto/UpdateNotificationSettingRequest.kt
@@ -0,0 +1,8 @@
+package org.depromeet.team3.notification.dto
+
+import jakarta.validation.constraints.NotNull
+
+data class UpdateNotificationSettingRequest(
+    @field:NotNull(message = "notificationEnabled 필드는 필수입니다.")
+    val notificationEnabled: Boolean
+)
