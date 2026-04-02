9853d751075f1c59f9bccb4bd1f7db72c40e3ff1
parkmineum <a01087164428@gmail.com>
2026-04-02 18:32:50 +0900

    perf(place): optimize google places api semaphore sizes and timeouts

--- ssolv-infrastructure/src/main/kotlin/org/depromeet/team3/common/GooglePlacesApiProperties.kt
+++ ssolv-infrastructure/src/main/kotlin/org/depromeet/team3/common/GooglePlacesApiProperties.kt
@@ -8,11 +8,11 @@ import org.springframework.stereotype.Component
 data class GooglePlacesApiProperties(
     var apiKey: String = "",
     var baseUrl: String = "https://places.googleapis.com",
-    var globalSemaphoreSize: Int = 15,
-    var requestSemaphoreSize: Int = 4,
+    var globalSemaphoreSize: Int = 12,
+    var requestSemaphoreSize: Int = 5,
     var totalFetchSize: Int = 10,
     var photoFallbackBuffer: Int = 5,
     var keywordFetchSize: Int = 20,
     var apiTimeoutMs: Long = 3000,
-    var semaphoreTimeoutMs: Long = 5000
+    var semaphoreTimeoutMs: Long = 3000
 )
